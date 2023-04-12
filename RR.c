2d464b9ee1a63dff5ed85cc24daf71d6e312f3ba
AhmedOsama198 <ahmed.helmy@bld.ai>
2023-04-12 22:18:49 +0200

    Finished First form of RR

--- RR.c
+++ RR.c
@@ -8,23 +8,25 @@ int quantum = 1 ;
 int main(int argc , char ** argv){
 
     struct Queue2 Processes ;
+    struct Queue2 Finished_Processes ;
+    Finished_Processes = createQueue();
     Processes =  createQueue() ;
     while (true){
        struct Queue2 Current_Quantum_Processes ;  // A temp queue
        // Inserting all the available processes in the quantum queue
-        while (!isEmpty(&Processes)) {
+        if (!isEmpty(&Processes)) {
             struct Process Current_Process = Processes.front->data;
             dequeue(&Processes) ;
             Current_Process.remRunTime -= min(Current_Process.remRunTime , quantum);
-            if(Current_Process.remRunTime){
-                enqueue(& Processes , Current_Process);
+            if(Current_Process.startingTime == -1){ // If it's the first time to get scheduled
+             Current_Process.startingTime =  getClk();
             }
-        }
-        while (!isEmpty(&Current_Quantum_Processes)){
-            dequeue(&Current_Quantum_Processes);
-            Current_Process.remRunTime -=
-
-
+            if(Current_Process.remRunTime){ // If it still didn't finish
+                enqueue(& Processes , Current_Process);
+            }else{
+                Current_Process.finishTime = getClk();
+                enqueue(& Finished_Processes , Current_Process);
+            }
         }
     }
 
