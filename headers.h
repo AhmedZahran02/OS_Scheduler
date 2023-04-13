9d7b5c95febe3d4460bb5c9b88a5f612623231cc
AhmedOsama198 <ahmed.helmy@bld.ai>
2023-04-14 01:14:49 +0200

    Some Edits

--- headers.h
+++ headers.h
@@ -128,7 +128,7 @@ void ProcessFinished(Process process)
     fclose(file_ptr);
 }
 int StartProcess(Process * P) {
-    printf("At time %d process %d started arr: %d total: %d remaining: %d wait: \n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime);
+    printf("At time %d process %d STARTED arr: %d total: %d remaining: %d wait: \n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime);
     int Process_Id = fork();
     if (Process_Id == 0)
     {
@@ -146,15 +146,15 @@ void FinishProcess(Process * P) {
     int TA = P->finishTime - P->arrivalTime ;
     double  WTA = TA / P->runTime;
 //    kill(P->realID , SIGKILL);
-    printf("At time %d process %d finished arr: %d total: %d remaining: %d wait: %d TA: %d WTA : %f\n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime , wait , TA ,WTA );
+    printf("At time %d process %d FINISHED arr: %d total: %d remaining: %d wait: %d TA: %d WTA : %f\n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime , wait , TA ,WTA );
 }
 void StopProcess(Process * P){
     int wait = P->startingTime - P->arrivalTime ;
-    printf("At time %d process %d stopped arr: %d total: %d remaining: %d wait: %d \n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime , wait );
+    printf("At time %d process %d STOPPED arr: %d total: %d remaining: %d wait: %d \n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime , wait );
     kill(P->realID, SIGSTOP);
     P->realID = -1;
 }
 void ContinueProcess(Process * P){
-    printf("process %d cont at time %d \n", P->id, getClk());
+    printf("At time %d process %d CONTINUED arr: %d total: %d remaining: %d  \n", getClk() , P->id ,P->arrivalTime ,  P->runTime , P->remRunTime  );
     kill(P->realID, SIGCONT);
 }
